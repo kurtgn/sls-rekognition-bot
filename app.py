47e51a0cfaefea2ae1882d978bd095099787b69d
Mike <mikhail.g.novikov@gmail.com>
2019-10-21 21:42:38 +0300

    fixed generating absolute url for top selfies

--- app.py
+++ app.py
@@ -27,9 +27,9 @@ def get_position_in_top(emotion: Emotion):
     )
 
 
-def get_link_for_top_emotion(emotion_type: str) -> str:
+def get_link_for_top_emotion(emotion_type: str, host: str) -> str:
     link = (
-        os.environ.get('LAMBDA_ENDPOINT', '')
+        host.rstrip('/')
         + url_for('top_for_emotion')
         + '?emotion='
         + emotion_type
@@ -84,13 +84,11 @@ def hello():
     position = get_position_in_top(emotion=top_emotion)
 
     text = utils.emotions_summary(emotions)
-
+    link = get_link_for_top_emotion(top_emotion.type, host=request.host_url)
     text += (
         f'\n\n👌 You are *#{position} {top_emotion.type} person* today!'
-        f'\nView all {top_emotion.type} people: '
-        f'{get_link_for_top_emotion(top_emotion.type)}'
+        f'\nView all {top_emotion.type} people: {link}'
     )
-
     utils.bot.send_text_message(chat_id=chat_id, text=text)
 
     s3_url = utils.upload_to_s3(url=picture_url, content=res.content)
